9216a40eef980d1df4de25d1240c40a6364e3dd2
EunHyunsu <ehs208@naver.com>
2025-01-13 09:48:01 +0900

    퀴즈 1.1 풀이

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,21 +3,33 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> hobbiesStream = csvLines.stream()
+            .map(line -> line[1])
+            .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+            .collect(Collectors.groupingBy(
+                hobby -> hobby.trim(),
+                Collectors.collectingAndThen(Collectors.counting(), Long::intValue)
+            ));
+        return hobbiesStream;
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
+
         return new HashMap<>();
     }
 
